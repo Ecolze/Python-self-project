7bf9af468e84ad1f9513bc5c6e3f2b9c5c902439
Ecolze <alfarizijambu54@gmail.com>
2025-05-01 19:36:08 +0700

    Add files via upload

---
+++ paswordgen.py
@@ -0,0 +1,18 @@
+# membuat pasword gen
+
+import random
+
+Up = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+low = "abcdefghijklmopqrsyuvwxyz"
+num = "1234567890"
+sys = "_."
+
+length = 8
+iden = Up+low+num+sys
+
+username ="".join(random.sample(iden, length))
+if iden == length: # membuat pengecualian untuk user
+    print("Invalid")
+else:
+    print(f"Pasword Anda adalah: {username}")
+        
